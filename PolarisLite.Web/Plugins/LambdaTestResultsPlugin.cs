dd52f6a8c11e392a74ef1c6d5be4f051c8eccdd0
Anton Angelov <aangelov@automatetheplanet.com>
2024-06-21 18:31:33 +0300

    add LambdaTestResultsPlugin

---
+++ PolarisLite.Web/Plugins/LambdaTestResultsPlugin.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium.Support.Extensions;
+using PolarisLite.Core;
+using PolarisLite.Core.Plugins;
+using PolarisLite.Web;
+using PolarisLite.Web.Plugins.BrowserExecution;
+using System.Reflection;
+namespace Bellatrix.Web.Plugins.Browser;
+
+public class LambdaTestResultsPlugin : Plugin
+{
+    public override void OnAfterTestCleanup(TestOutcome testResult, MethodInfo memberInfo, Exception failedTestException)
+    {
+        var driver = DriverFactory.WrappedDriver;
+
+        var executionType = ConfigurationService.GetSection<WebSettings>().ExecutionType;
+        bool isLambdaTestRun = executionType.Equals("lambdatest", StringComparison.OrdinalIgnoreCase);
+
+        try
+        {
+            if (isLambdaTestRun && testResult == TestOutcome.Passed)
+            {
+                driver.ExecuteJavaScript("lambda-status=passed");
+            }
+            else if (isLambdaTestRun)
+            {
+                // pass real exception to LambdaTest
+                driver.ExecuteJavaScript("lambda-exceptions", failedTestException);
+                driver.ExecuteJavaScript("lambda-status=failed");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
