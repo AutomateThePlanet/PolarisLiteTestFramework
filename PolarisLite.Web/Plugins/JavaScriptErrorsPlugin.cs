d2cf24e9e92fc1b59eb50003fc0b8ec8cd8e4f81
Anton Angelov <aangelov@automatetheplanet.com>
2024-06-21 18:11:26 +0300

    add new JavaScriptErrorsPlugin

---
+++ PolarisLite.Web/Plugins/JavaScriptErrorsPlugin.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using PolarisLite.Core;
+using PolarisLite.Core.Plugins;
+using PolarisLite.Web.Plugins.BrowserExecution;
+using System.Collections.ObjectModel;
+using System.Reflection;
+namespace Bellatrix.Web.Plugins.Browser;
+
+public class JavaScriptErrorsPlugin : Plugin
+{
+    public override void OnAfterTestCleanup(TestOutcome result, MethodInfo memberInfo, Exception failedTestException)
+    {
+        // TODO: refactor DriverFactory to collect logs
+        // chromeOptions.SetLoggingPreference(LogType.Browser, LogLevel.All);
+        var driver = DriverFactory.WrappedDriver;
+        //bool shouldCheckForJsErrors = ConfigurationService.GetSection<WebSettings>().ShouldCheckForJavaScriptErrors;
+        //if (driver == null || !shouldCheckForJsErrors)
+        //{
+        //    return;
+        //}
+
+        var errorStrings = new List<string>
+         {
+             "SyntaxError",
+             "EvalError",
+             "ReferenceError",
+             "RangeError",
+             "TypeError",
+             "URIError",
+         };
+        ReadOnlyCollection<OpenQA.Selenium.LogEntry> browserLogs = driver?.Manage()?.Logs?.GetLog(LogType.Browser);
+        var jsErrors = browserLogs?.Where(x => errorStrings.Any(e => !string.IsNullOrEmpty(x.Message) && x.Message.Contains(e)));
+        if (jsErrors != null && jsErrors.Any())
+        {
+            Assert.Fail($"JavaScript error(s): {Environment.NewLine} {jsErrors.Aggregate(string.Empty, (s, entry) => s + entry.Message)}{Environment.NewLine}");
+        }
+    }
+}
