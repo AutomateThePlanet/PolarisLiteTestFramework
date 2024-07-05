675b12577f537a477ab1bd839f66ab51684334ff
Anton Angelov <aangelov@automatetheplanet.com>
2024-07-05 17:22:19 +0300

    updates

--- PolarisLite.Mobile/Services/DriverAdapter.cs
+++ PolarisLite.Mobile/Services/DriverAdapter.cs
@@ -12,20 +12,11 @@ public partial class DriverAdapter
     private IWebDriver _webDriver;
     private WEB.DriverAdapter _webDriverAdapter;
     private Actions _actions;
-    private List<WaitStrategy> _waitStrategies;
 
     public DriverAdapter()
     {
         _androidDriver = DriverFactory.WrappedAndroidDriver;
         _webDriverAdapter = new WEB.DriverAdapter(_androidDriver);
-        //_driverWait = new AndroidDriverWait(_androidDriver, new SystemClock(), TimeSpan.FromSeconds(30), TimeSpan.FromSeconds(2));
-        _waitStrategies = new List<WaitStrategy>();
-        _waitStrategies.Add(new ToExistWaitStrategy());
         _actions = new Actions(_androidDriver);
-    }
-
-    public void EnsureState(WaitStrategy waitStrategy)
-    {
-        _waitStrategies.Add(waitStrategy);
     }
 }
